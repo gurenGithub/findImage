cb8ebb6c699a9444a690c23bf13e0185dae48728
guren_d <guren_d@hotmail.com>
2019-04-18 14:21:46 +0800

    333

--- core/index.js
+++ core/index.js
@@ -65,6 +65,21 @@ var getKey = function (x, y) {
 
     return x + splitStr + y;
 }
+
+var logTime=function(_startDate,title){
+
+
+    var _endDate=new Date();
+    console.log({
+        title:title,
+        //result: result,
+        startDate: _startDate,
+        endDate: _endDate,
+        total: _endDate.getTime() - _startDate.getTime(),
+    });
+
+    return _endDate;
+}
 /**
  * 
  * @param {原创图片} source 
@@ -74,66 +89,51 @@ var getKey = function (x, y) {
  * @param {结束查找坐标:x1} x1 
  * @param {结束查找坐标:y2} y2 
  */
-const find = function (source, compare, x, y, x1, y1) {
+const find = function ({source, compare, x, y, x1, y1,onCompleted}) {
 
 
-
+    let scaleDiv=2;
+    let scaleParams=1/scaleDiv;
+    let _startDate = new Date();
     Jimp.read(source, function (err, sourceImage) {
 
-         sourceImage.greyscale((err, sourceImage) => {
-        var sourceP = getProperties(sourceImage);
+
+        var sourcePoints = [];
+       _startDate= logTime(_startDate,'读取');
+
+       sourceImage.scale(scaleParams);
+        sourceImage.greyscale((err, sourceImage) => {
+         _startDate=   logTime(_startDate,'置灰');
+            var sourceP = getProperties(sourceImage);
 
 
-        x = x || 0;
-        y = y || 0;
-        x1 = x1 || sourceP.width;
-        y1 = y1 || sourceP.height;
-        //console.log(sourceP);
-        Jimp.read(compare, function (err, compareImage) {
+           // console.log(sourceP);
 
-            compareImage.greyscale((err, compareImage) => {
-            var compareP = getProperties(compareImage);
-
-
-
-
-            //console.log(cropImage);
-
-            // return;
-
+            x = x || 0;
+            y = y || 0;
+            x1 = x1 || sourceP.width;
+            y1 = y1 || sourceP.height;
 
             var x2 = x1;
             var y2 = y1;
 
-            var startX = 0;
-            var endY = 0;
 
-            var startDate = new Date();
-            console.log(startDate, sourceP);
+            for (let i = x; i < x2; i++) {
 
-
-            var comparePoints = []
-
-            var sourcePoints = {};
-            var key;
-
-            console.log(x, y);
-            for (var i = x; i < x2; i++) {
-
-                for (var j = y; j < y2; j++) {
+                for (let j = y; j < y2; j++) {
 
 
-                    var newX = i;
-                    var newY = j;
+                    let newX = i;
+                    let newY = j;
                     //var cropImage=  sourceImage.crop(newX,newY,compareP.width,compareP.height);
                     // sourceImage.crop(newX,newY,compareP.width,compareP.height);
 
-                    // var diff = Jimp.diff(sourceImage, cropImage); // pixel difference
+                    //var diff = Jimp.diff(sourceImage, cropImage); // pixel difference
 
-                    var pixel = sourceImage.getPixelColor(newX, newY); // returns the colour of that pixel e.g. 0xFFFFFFFF
+                    let pixel = sourceImage.getPixelColor(newX, newY); // returns the colour of that pixel e.g. 0xFFFFFFFF
                     //Jimp.intToRGBA(pixel); 
                     //console.log(pixel);
-                    var rgba = Jimp.intToRGBA(pixel);
+                    let rgba = Jimp.intToRGBA(pixel);
 
                     if (!sourcePoints[i]) {
                         sourcePoints[i] = [];
@@ -147,124 +147,224 @@ const find = function (source, compare, x, y, x1, y1) {
             }
 
 
-            for (var ci = startX; ci < compareP.width; ci += 1) {
+           _startDate= logTime(_startDate,'转换GRBA');
+            var callback = function (compare, fn) {
 
-                for (var cj = endY; cj < compareP.height; cj += 1) {
 
-
-                    var comparePixel = compareImage.getPixelColor(ci, cj); // returns the colour of that pixel e.g. 0xFFFFFFFF
-                    //Jimp.intToRGBA(pixel); 
-                    //console.log(pixel);
-                    var rgba = Jimp.intToRGBA(comparePixel);
-                    if (!comparePoints[ci]) {
-                        comparePoints[ci] = [];
-                    }
-                    comparePoints[ci][cj] = rgba;
+                //console.log(sourceP);
+                Jimp.read(compare, function (err, compareImage) {
+                    compareImage.scale(scaleParams );
+                    compareImage.greyscale((err, compareImage) => {
+                        var compareP = getProperties(compareImage);
 
 
 
 
-                }
+                        //console.log(cropImage);
+
+                        // return;
 
 
-            }
+                        var startX = 0;
+                        var endY = 0;
+
+                        var startDate = new Date();
+                       // console.log(startDate, sourceP);
+
+
+                        var comparePoints = []
+
+
+                        var key;
+
+                       // console.log(x, y);
+                       
+
+
+                        for (var ci = startX; ci < compareP.width; ci += 1) {
+
+                            for (var cj = endY; cj < compareP.height; cj += 1) {
+
+
+                                var comparePixel = compareImage.getPixelColor(ci, cj); // returns the colour of that pixel e.g. 0xFFFFFFFF
+                                //Jimp.intToRGBA(pixel); 
+                                //console.log(pixel);
+                                var rgba = Jimp.intToRGBA(comparePixel);
+                                if (!comparePoints[ci]) {
+                                    comparePoints[ci] = [];
+                                }
+                                comparePoints[ci][cj] = rgba;
 
 
 
 
-
-            var count = 0;
-            var total =0;
-            var lastPoints = {};
-
-
-           // x = 10, x2 = 11, y = 10, y2 = 11;
-            for (var i = x; i < x2; i++) {
-
-                for (var j = y; j < y2; j++) {
-
-
-
-
-
-
-                    if (!isSame(comparePoints[0][0], sourcePoints[i][j])) {
-                        //lastPoints={};
-                        //isbreak = true;
-                        continue;
-                    }
-
-                     total = compareP.width * compareP.height;
-                    count = 0;
-                    for (var ci = startX; ci < compareP.width; ci++) {
-
-
-                        var isbreak = false;
-                        for (var cj = endY; cj < compareP.height; cj++) {
-
-
-                            var comparePoint = comparePoints[ci][cj];
-
-                            point = sourcePoints[i + ci][j + cj];
-
-                            //console.log(comparePoint,point,{ci:ci,cj:cj,i:i ,j:j});
-
-                            if (!point) {
-
-                                //lastPoints={};
-                               // console.log(comparePoint, point);
-                                isbreak = true;
-                                break;
-                            }
-                            if (!isSame(comparePoint, point)) {
-                                //lastPoints={};
-                                /*console.log(comparePoint, point, {
-                                    ci: ci,
-                                    cj: cj,
-                                    i: i + ci,
-                                    j: j + cj
-                                });*/
-                                isbreak = true;
-                                break;
                             }
 
-                            count++;
+
                         }
-                        if (isbreak) {
-                            break;
-                        }
-                    }
 
 
-                    //console.log(total, count);
-                    if (count >= total) {
-                      
-                        total=count;
-                        lastPoints = {
-                            x: i,
-                            y: j
+
+
+
+                        var count = 0;
+                        var total = 0;
+                        var lastPoints = {
+                            x: -1,
+                            y: -1
                         };
-                    }
-                    //console.log('第一个相似坐标:'+count,{x:i,y:j});
-                }
 
+                        var __width=compareP.width-1;
+                        var __height=compareP.height-1;
+                        // x = 10, x2 = 11, y = 10, y2 = 11;
+                        for (var i = x; i < x2-__width; i++) {
+
+                            for (var j = y; j < y2-__height; j++) {
+
+
+
+
+
+                                
+
+                               // break;
+
+                                if (!isSame(comparePoints[0][0], sourcePoints[i][j])
+                               ) {
+                                    //lastPoints={};
+                                    //isbreak = true;
+                                    continue;
+                                }
+
+                                total = compareP.width * compareP.height;
+                                count = 0;
+                                for (var ci = startX; ci < compareP.width; ci++) {
+
+
+                                    var isbreak = false;
+                                    for (var cj = endY; cj < compareP.height; cj++) {
+
+
+                                        var comparePoint = comparePoints[ci][cj];
+
+                                        point = sourcePoints[i + ci][j + cj];
+
+                                        //console.log(comparePoint,point,{ci:ci,cj:cj,i:i ,j:j});
+
+                                        if (!point) {
+
+                                            //lastPoints={};
+                                            // console.log(comparePoint, point);
+                                            isbreak = true;
+                                            //continue;
+                                            break;
+                                        }
+                                        if (!isSame(comparePoint, point)) {
+                                            //lastPoints={};
+                                            /*console.log(comparePoint, point, {
+                                                ci: ci,
+                                                cj: cj,
+                                                i: i + ci,
+                                                j: j + cj
+                                            });*/
+                                            isbreak = true;
+                                            //continue;
+                                            break;
+                                        }
+
+                                        count++;
+                                    }
+                                    if (isbreak) {
+                                       // continue;
+                                        break;
+                                    }
+                                }
+
+
+                                //console.log(total, count);
+                                if (count >= total) {
+
+                                    total = count;
+                                    lastPoints = {
+                                        x: i,
+                                        y: j
+                                    };
+                                }
+                                //console.log('第一个相似坐标:'+count,{x:i,y:j});
+                            }
+
+                        }
+
+                        //console.log('相似:' + total, lastPoints);
+                        var endDate = new Date();
+                        //console.log(endDate, endDate.getTime() - startDate.getTime());
+                        // console.log(sourcePoints, sourcePoints);
+
+
+
+                        //console.log(Jimp.intToRGBA(compareImage.getPixelColor(0, 0)), comparePoints[0][0]);
+                        //console.log(Jimp.intToRGBA(sourceImage.getPixelColor(10, 10)), sourcePoints[10][10]);
+
+                        if (fn) {
+
+                            fn(lastPoints);
+                        }
+                    })
+
+                })
             }
 
-            console.log('相似:' + total, lastPoints);
-            var endDate = new Date();
-            console.log(endDate, endDate.getTime() - startDate.getTime());
-            // console.log(sourcePoints, sourcePoints);
 
 
+            var paths = compare.split(',');
 
-            console.log(Jimp.intToRGBA(compareImage.getPixelColor(0, 0)), comparePoints[0][0]);
-            console.log(Jimp.intToRGBA(sourceImage.getPixelColor(10, 10)), sourcePoints[10][10]);
+
+            var path = paths.shift();
+          
+
+
+            // console.log(path);
+            var result = [];
+
+            var _endFn = function () {
+
+                var _endDate = new Date();
+
+                console.log({
+                    title:'对比',
+                    result: result,
+                    startDate: _startDate,
+                    endDate: _endDate,
+                    total: _endDate.getTime() - _startDate.getTime(),
+                });
+                if(onCompleted){
+                    onCompleted(result);
+                }
+            }
+            var _eachFind = ((el) => {
+
+                callback(path, function (data) {
+
+                    if (data.x != -1) {
+                        result.push(data);
+                    }
+
+                    path = paths.shift();
+                    if (path) {
+
+                        _eachFind(path);
+                    } else {
+                        _endFn();
+                    }
+
+                });
+            })
+
+            _eachFind(path);
+
         })
-
     })
 
-    })
-    })
 
 
 }
--- index.js
+++ index.js
@@ -1,22 +1,34 @@
 var core= require('./core/index');
 
 
-var sourcePath="assets/source2.jpeg";
-var comparePath="assets/compare.jpeg";
-/*
-core.read(sourcePath,function(image){
+var sourcePath="assets/source2.png";
+//var comparePath="assets/compare.jpeg,assets/compare1.jpeg,assets/compare2.jpeg,assets/compare3.jpeg,assets/compare4.jpeg,assets/compare6.jpeg";
+
+var comparePath="assets/compare.jpeg,assets/compare1.jpeg,assets/compare2.jpeg,assets/compare3.jpeg,assets/compare4.jpeg,assets/compare6.jpeg";
+
+let isSave=false;
 
 
-    console.log(image);
+if(isSave){
+    core.read(sourcePath,function(image){
+
+
+        console.log(image);
+        
+        core.crop(image,28,40,100,100);
     
-    core.crop(image,30,34,100,100);
-
+        
+        core.write(image,'assets/compare6.jpeg',function(res){
     
-    core.write(image,'assets/compare.jpeg',function(res){
-
-
+    
+        })
     })
-})
+    
+}else{
+    core.find({source:sourcePath,compare:comparePath,onCompleted(result){
 
-*/
-core.find(sourcePath,comparePath);
+
+    }});
+}
+
+
